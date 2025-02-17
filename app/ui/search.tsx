ac469c30a5005af96dca4f61c87e17dc453bd0d9
davidwanlongwu <2490050603@qq.com>
2025-02-16 22:54:51 -0700

    completed first phase of search-and-useSearchParams practice

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,26 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { log } from 'console';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  
+  const handleSearch = useDebouncedCallback((term: string | number) => {
+    const params = new URLSearchParams(searchParams.toString());
+    if(term){
+     params.set('query', term.toString())
+    } else {
+     params.delete('query')
+    }
+   console.log('Searching ...', term);
+   
+   return router.replace(`${pathname}?${params.toString()}`)
+},500)
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +29,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e)=>{handleSearch(e.target.value)}}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
